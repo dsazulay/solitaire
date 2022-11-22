78f0dc0e72d2d321857cde9a269ca2c2f21e0c70
Diego Azulay <diego.azulay@mailbox.org>
2022-11-22 19:34:18 +0100

    Move imgui files to external dir

--- src/ui_renderer.cpp
+++ src/ui_renderer.cpp
@@ -1,8 +1,8 @@
 #include "ui_renderer.h"
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include "timer.h"
 #include "utils/log.h"
 
